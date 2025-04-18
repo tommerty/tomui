8cd9f904eda8fa4404976eb1bba51f148222f3c3
Tommy Lundy <tommerty@github.com>
2025-04-18 10:39:44 +0100

    refactor: Type errors preventing build

--- lib/rehype-custom-slug.ts
+++ lib/rehype-custom-slug.ts
@@ -1,13 +1,14 @@
 import { visit } from "unist-util-visit";
 import { toString } from "hast-util-to-string";
-import type { Plugin } from "unified";
+import type { Plugin, Transformer } from "unified";
+import type { Element } from "hast";
 import type { Node } from "unist";
 
 // Custom rehype plugin to generate custom IDs for headings
 export function rehypeCustomSlug(): Plugin {
-    return function () {
-        return function (tree: Node) {
-            visit(tree, "element", (node: any) => {
+    return function plugin(): Transformer {
+        return function transformer(tree: Node): void {
+            visit(tree, "element", (node: Element) => {
                 if (node.tagName === "h1") {
                     const text = toString(node);
 
@@ -17,6 +18,7 @@ export function rehypeCustomSlug(): Plugin {
                     if (versionMatch) {
                         // Convert "1.0.0" to "1-0-0"
                         const version = versionMatch[1].replace(/\./g, "-");
+                        node.properties = node.properties || {};
                         node.properties.id = version;
                     }
                 }
